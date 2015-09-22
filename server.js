c761834932710fcf47dcf2e0151e8d241a93c811
Andrew Brown <arbrow06@syr.edu>
2015-09-22 14:39:52 -0400

    Initial Commit

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+
+apt.get('/', function (req, res) {
+	res.send('Hello World!);
+});
+var server = app.listen(3000, function(){
+	var host = server.address().address;
+	var port = server.address().port;
+
+	console.log('App listing at http://%s:%s', host, port);
+});
