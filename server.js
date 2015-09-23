f3370bb5c8ca6c9851764b6a182ea5fbdfa7ad9e
Andrew Brown <arbrow06@syr.edu>
2015-09-22 21:37:28 -0400

    Syntax and code errors corrected

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var app = express();
 
-apt.get('/', function (req, res) {
-	res.send('Hello World!);
+app.get('/', function (req, res) {
+	res.send('Hello World!');
 });
 var server = app.listen(process.env.ADDRESS || 3000, function(){
 	var host = server.address().address;
