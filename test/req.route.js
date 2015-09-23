ead152ecb37034b2d15e5726f355f712d7fecf2f
Andrew Brown <arbrow06@syr.edu>
2015-09-22 21:06:33 -0400

    New test cases and dev dependencies

---
+++ test/req.route.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var request = require('supertest');
+
+describe('req, function(){
+  describe('.route', function(){
+    it('should be the executed route', function(done){
+      var app = express();
+
+      app.get('/', function(req, res,next){
+        req.route.path.should.equal('/');
+	next();
+      });
+      request(app)
+      .get('/')
+      .expect(200, done);
+    });
+  });
+});
